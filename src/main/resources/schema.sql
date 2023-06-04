bc51f077e992085083c11e60336d00bdfaa45d4b
Roland San Luis <rsanluis@gmail.com>
2023-06-04 01:44:30 -0400

    adding books table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,13 @@
--- Create virtual table Wog (Word of God)
-create virtual table if not exists WoG using fts5(b,c,v,s);
+-- Drop virtual table WoG (Word of God)
+drop table if exists WoG;
+
+-- Create virtual table WoG (Word of God)
+create virtual table WoG using fts5(b,c,v,s);
+
+-- Create table Books
+create table if not exists Books (
+    id integer not null primary key,
+    title text not null,
+    short_title text not null,
+    chapters integer not null
+)
