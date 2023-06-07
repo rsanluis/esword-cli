3c83d0176e6b8416249c9fe726290f91c53dc51d
Roland San Luis <rsanluis@gmail.com>
2023-06-06 22:27:24 -0400

    adding test for vision

---
+++ src/test/java/com/codified/esword/dao/SearchDAOImpl_86_Matches_Test.java
@@ -0,0 +1,85 @@
+package com.codified.esword.dao;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.codified.esword.model.SearchResult;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+public class SearchDAOImpl_86_Matches_Test {
+
+    @Autowired
+    SearchDAO searchDAO;
+
+    @Test
+    void testSearchByKeyword_H3925_AndContext_OldTestament() {
+        List<SearchResult> resultsList = searchDAO.searchByKeywordAndContext("H3925", "OldTestament");
+        int verses  = 0;
+        int matches = 0;
+        for (SearchResult searchResult : resultsList) {
+            matches += StringUtils.countMatches(searchResult.getScripture(), SearchDAO.MATCH_START_TAG);
+        }
+        verses = resultsList.size();
+        log.info("verses:  {}", verses);
+        log.info("matches: {}", matches);
+        assertTrue(verses == 80);
+        assertTrue(matches == 86);
+    }
+
+    @Test
+    void testSearchByKeyword_G2540_AndContext_OldTestament() {
+        List<SearchResult> resultsList = searchDAO.searchByKeywordAndContext("G2540", "NewTestament");
+        int verses  = 0;
+        int matches = 0;
+        for (SearchResult searchResult : resultsList) {
+            matches += StringUtils.countMatches(searchResult.getScripture(), SearchDAO.MATCH_START_TAG);
+        }
+        verses = resultsList.size();
+        log.info("verses:  {}", verses);
+        log.info("matches: {}", matches);
+        assertTrue(verses == 81);
+        assertTrue(matches == 86);
+    }
+
+    @Test
+    void testSearchByKeyword_Vision_Visions_AndContext_OldTestament() {
+        List<SearchResult> resultsList = searchDAO.searchByKeywordAndContext("vision", "OldTestament");
+        int verses  = 0;
+        int matches = 0;
+        int matchesForVision  = 0;
+        int matchesForVisions = 0;
+        for (SearchResult searchResult : resultsList) {
+            matches += StringUtils.countMatches(searchResult.getScripture(), SearchDAO.MATCH_START_TAG);
+        }
+        verses = resultsList.size();
+        log.info("verses:  {}", verses);
+        log.info("matches: {}", matches);
+        assertTrue(verses == 58);
+        assertTrue(matches == 64);
+        matchesForVision = matches;
+        verses  = 0;
+        matches = 0;
+        resultsList = searchDAO.searchByKeywordAndContext("visions", "OldTestament");
+        for (SearchResult searchResult : resultsList) {
+            matches += StringUtils.countMatches(searchResult.getScripture(), SearchDAO.MATCH_START_TAG);
+        }
+        verses = resultsList.size();
+        log.info("verses:  {}", verses);
+        log.info("matches: {}", matches);
+        matchesForVisions = matches;
+        assertTrue(verses == 22);
+        assertTrue(matches == 22);
+        // 64 + 22 = 86 total matches for vision/visons in the Old Testament
+        assertTrue((matchesForVision + matchesForVisions) == 86);
+    }
+}
+
