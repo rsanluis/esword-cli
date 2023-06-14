cbbef2df2873ffdad8ced6cbe3e7f9a62f67e4a3
Roland San Luis <rsanluis@gmail.com>
2023-06-14 11:37:43 -0400

    adding test for 200 matches - for our Reysh, our prince, the God head, which is Jesus Christ

--- src/test/java/com/codified/esword/dao/WordMatchesTest.java
+++ src/test/java/com/codified/esword/dao/WordMatchesTest.java
@@ -94,6 +94,19 @@ public class WordMatchesTest {
     assertTrue(wordMatchesList.get(0).getMatches()==3);
   }
 
+  /*
+    200 is the ancient Hebrew code for Reysh that signifies the head person or Prince.
+    https://letterpile.com/religion/Berisheet-Our-New-Beginning
+  */
+  @Test
+  public void testWordMatchesBible_200_Matches_Test() {
+    List<WordMatches> wordMatchesList = wordMatchesBibleDAO.getWordMatchesByMatches(200);
+    log.info("wordMatchesList.size: {}", wordMatchesList.size());
+    assertTrue(wordMatchesList.size()==1);
+    assertTrue(wordMatchesList.get(0).getWord().equals("h3519"));
+    // All uses of H3519 in the entire KJV+ Bible: {glory,glorious,gloriously,honour,honourable}
+  }
+
   @Test
   public void testWordMatchesBible_555_Matches_Test() {
     List<WordMatches> wordMatchesList = wordMatchesBibleDAO.getWordMatchesByMatches(555);
