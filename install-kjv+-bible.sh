4fc337002cb2aece07cdcbdce90711d5e417449d
Roland San Luis <rsanluis@gmail.com>
2023-06-23 18:39:13 -0400

    modying install db script

--- install-kjv+-bible.sh
+++ install-kjv+-bible.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-if [ ! -f "kjv.zip" ]; then
-  curl https://codified.com/kjv.zip -o kjv.zip
+ESWORD_CLI_DIR=../esword-db
+pushd .
+if [ -d "$ESWORD_CLI_DIR" ];
+then
+  echo "$ESWORD_CLI_DIR directory exists."
+  cd ..
+else
+  echo "$ESWORD_CLI_DIR directory does not exist."
+  cd ..
+  git clone https://github.com/rsanluis/esword-db.git
 fi
-unzip -o kjv.zip
+cd esword-db
+./build.sh
+popd
+cp ../esword-db/*.bbli .
 
+#Fetch DB method #2
+#if [ ! -f "kjv.zip" ]; then
+#  curl https://codified.com/kjv.zip -o kjv.zip
+#fi
+#unzip -o kjv.zip
+
+#Fetch DB method #3 - regenerate the DB from kjv+.bbli file
 #Comment out the above lines then uncomment the below lines if you're running a mac to recreate the database from the baseline kjv+.dzip file
 #Note: You will need to have e-Sword installed on your mac prior to running this.
 
