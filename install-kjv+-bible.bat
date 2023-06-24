8fe6c4096080d3c36146399e8c426debc32ecc54
Roland San Luis <rsanluis@gmail.com>
2023-06-24 09:37:00 -0400

    modifying install db script for windows

--- install-kjv+-bible.bat
+++ install-kjv+-bible.bat
@@ -1,1 +1,12 @@
-powershell -ExecutionPolicy ByPass -File install-kjv+-bible.ps1
+@echo off
+set CURDIR=%~dp0
+set ESWORD_DB_DIR=%CURDIR%..\esword-db\
+pushd .
+if not exist %ESWORD_DB_DIR% (
+  cd %curdir%..
+  git clone https://github.com/rsanluis/esword-db.git
+)
+cd %ESWORD_DB_DIR%
+call build.bat
+popd
+xcopy /Y %ESWORD_DB_DIR%kjv+matches.bbli %CURDIR%
--- install-kjv+-bible.ps1
+++
@@ -1,6 +0,0 @@
-$client = new-object System.Net.WebClient
-$fpath = "kjv.zip"
-if(-not(Test-path $fpath -PathType leaf)) {
-    $client.DownloadFile("https://codified.com/$fpath", $fpath)
-} 
-jar xvf $fpath
