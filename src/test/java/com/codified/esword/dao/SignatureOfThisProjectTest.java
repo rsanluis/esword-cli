32daa08af7856136e7006f1a34c1185971aea4b3
Roland San Luis <rsanluis@gmail.com>
2023-07-24 23:52:29 -0400

    adding isaiah 46:9-11 commentary

--- src/test/java/com/codified/esword/dao/SignatureOfThisProjectTest.java
+++ src/test/java/com/codified/esword/dao/SignatureOfThisProjectTest.java
@@ -32,4 +32,49 @@ public class SignatureOfThisProjectTest {
         log.info("hebrew gematria value for {}: {}", name, hebrewGematriaValue);
         assertTrue(hebrewGematriaValue == 1290);
     }
+
+    @Test
+    void testIsaiah46_9_11() {
+        //
+        // This is a true story worth re-telling, otherwise it will remain forever between myself and God
+        //
+        // Within the Year(s):Month(s) away from my birthday 05/13/1976
+        //
+        // 46th year:9th month - At this time, I had already derived of the 11/01/2023 date for the 
+        //         destruction of Babylon and 12/06/2023 for being the start of tribulation.
+        //         You can verify the timestamps of the timeline I created based on Daniel & Revelation:
+        //         https://time.graphics/line/566988
+        //         I fasted and prayed for 10 days (03/01/2023-03/10/2023) with incense; asking God
+        //         "Should I show this info to his people?"  I told him, "They won't ever believe me"
+        //
+        // 46th year:10th month - He showed me other verses that instructed me to utter this info to his people, 
+        //         no matter if they would like to hear me or not.
+        //         Isaiah 48:20 Go ye forth of Babylon, flee ye from the Chaldeans, with a voice of singing declare ye, 
+        //         tell this, utter it even to the end of the earth; say ye, The LORD hath redeemed his servant Jacob.
+        //
+        //         I followed his advice (counsel) and did the video,
+        //         The Exact Date of Babylon's Destruction by Flood (Revealed) on Brighteon.com
+        //         on 04/05/2023 (210 days away from D-day)
+        //
+        // 46th year:11th month - By the end of April, I was shown the verses in Isaiah (46:9-11) matching
+        //         this exact timing of finding God's true timeline, the prayer & fasting, and his advice to
+        //         tell his people. It was no coincidence. God is real and listens to our prayers and thoughts.  
+        //         This was God telling me that the timeline I have (https://time.graphics/line/566988)
+        //         is correct.
+        //
+        // Isaiah 46:11 - This same verse holds different meanings for each time period.
+        //         The east in the past was Persia and Cyrus was called by name.
+        //         The east in the present day meaning is the far east which could mean any of the far east countries.
+        //         God narrowed the country by specifying ravenous bird or bird of prey.
+        //         There is currently one rare bird of the east and that is only found in the country of the Philippines.
+        //         https://en.wikipedia.org/wiki/Philippine_eagle
+        //         God was using that verse to show that my bloodlines were Filipino, althouth I was born & raised in the U.S.
+        //         I fled the U.S. in 2022 due to what I know after deciphering the verses in Revelation.
+        //         As I'm calling him by his number and name, he is also calling me via number:
+        //         1290, as this is the number of God's prophetic timeline.
+        //         1290 / 30 = 43   (4+3) = 7 # for God
+        //         1260 / 30 = 42   (4+2) = 6 # for Man
+        //         I'm now doing God's bidding from a far country, and he's using me as an instrument to bring about 
+        //         his plan and timeline for humanity.
+    }
 }
