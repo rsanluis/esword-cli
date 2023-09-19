54975c01bf1e790a676366aa752b7f3bf045054d
Roland San Luis <rsanluis@gmail.com>
2023-09-19 16:51:29 -0400

    refactoring

--- src/test/java/com/codified/esword/dao/Event_2007_09_30_Test.java
+++ src/test/java/com/codified/esword/dao/Event_2007_09_30_Test.java
@@ -15,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @SpringBootTest
 public class Event_2007_09_30_Test {
+    //
     // Event 09/30/2007 - September 30th, 2007 - The Four Horsemen meet in Washington, D.C.
     //
     // On 30 September 2007, Dawkins, Harris, Hitchens and Dennett met at Hitchens' residence
@@ -24,7 +25,16 @@ public class Event_2007_09_30_Test {
     // as the "Four Horsemen of the Non-Apocalypse", an allusion to the biblical Four Horsemen
     // of the Apocalypse from the Book of Revelation. The four have been described as 
     // "evangelical atheists".
+    //
+    // The Four Horsemen HD: Hour 1 of 2 - Discussions with Richard Dawkins, Ep 1
+    // https://youtu.be/9DKhc1pcDFM?si=8wB9dLZLLQMPqUob
+    // The Four Horsemen: Hour 2 of 2 - Discussions with Richard Dawkins, Ep 1
+    // https://youtu.be/TaeJf-Yia3A?si=eYSxrMbMvz_3LEW1
+    //
     // https://web.archive.org/web/20230829061516/http://en.wikipedia.org:80/wiki/New_atheism
     // 
+    // Man has written that these Four are not    the Four Horsemen of the Apocalypse
+    // God has written that these Four are indeed the Four Horsemen of the Apocalypse
+    //
     // 
 }
