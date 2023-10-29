9d08a46b8f1d58a376ce1db79435173490e95a79
Roland San Luis <rsanluis@gmail.com>
2023-10-28 22:36:16 -0400

    ref

--- src/test/java/com/codified/esword/dao/NthRootTest.java
+++ src/test/java/com/codified/esword/dao/NthRootTest.java
@@ -22,6 +22,9 @@ public class NthRootTest {
     //    60   10
     //   600  100
     //
+    // I noticed that every 3rd iteration formed the above pattern of 1, 10, 100
+    // https://github.com/rsanluis/esword-cli/blob/main/images/nthroot/every_3rd_iteration_grows_by_a_factor_of_10.png
+    //
     // The 3-6-9.sh  script in the nthroot project represents the Pattern of Man
     // The 3-7-10.sh script in the nthroot prpject represents the Pattern of God
     // 
@@ -30,7 +33,9 @@ public class NthRootTest {
     // The power of ordinal value of 10 is evident in the chart to the left
     // It starts with 1, and then starts with 10 and keeps repeating 10
     //
-    // This chart was also crucially important in solving the 2000 year riddle of the mark of the beast
+    // This chart was also crucially important in solving the 2000 year riddle of the Mark of the Beast
+    // Look at TheMarkOfTheBeastTest.java to see how I solved the formula.
+    // Revelation 13:18 is actually a formula to solve.
     //
     // Revelation 1:8
     // I  A         M 
@@ -40,5 +45,10 @@ public class NthRootTest {
     //
     // It later struck me that it matches the pattern for I AM
     // It continually repeats with the value of 10, like how the digits of PI keeps repeating forever.
+
+
+    // Daniel 3:25  He answered and said, Lo, I see four men loose, walking in the midst of the fire, 
+    // and they have no hurt; and the form of the fourth is like the Son of God. 
+
     
 }
