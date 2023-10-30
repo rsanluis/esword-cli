3ad27a9503c226f863035682b33da294d5657553
Roland San Luis <rsanluis@gmail.com>
2023-10-30 12:38:58 -0400

    ref

--- src/test/java/com/codified/esword/dao/Pattern35_ACertainManWasSickTest.java
+++ src/test/java/com/codified/esword/dao/Pattern35_ACertainManWasSickTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class Pattern35_ACertainManWasSickTest {
     // John 11:1  Now a certain man was ill, Lazarus of Bethany, the village of Mary and her sister Martha. 
     // https://rumble.com/v1aui9z-edward-dowd-covid-vaccines-causing-explosive-death-and-disability.html
+    // https://github.com/rsanluis/esword-cli/blob/main/images/asleep_in_truth/fraud_to_end_all_frauds.png
     //
     // Lazarus in the End Times represents the certain (group of Men) that is currently sick,
     // dead in sleep, or metaphorically asleep in the truth while awake.
@@ -17,17 +18,29 @@ public class Pattern35_ACertainManWasSickTest {
     //
     // 1) Jesus stated in John 10:30, I and the Father are one.
     //    If God and Jesus are one, then Jesus definitely knows the future.
-    //    a) For example, when he flipped the tables of the money changers he knew
+    //    a) When Jesus flipped the tables of the money changers he knew
     //      that money in the future will be an issue of the future; take for instance
-    //      the notion of a cashless society and programmable currency
+    //      the introduction of a cashless society and programmable currency like CBDCs
+    //      https://www.youtubetrimmer.com/view/?v=boNIL1owRAI&start=48&end=61&loop=0
     //      
     //      Jesus Cleanses the Temple
     //      Mathew 21:12  And Jesus entered the temple and drove out all who sold and bought
-    //      in the temple, and he overturned the tables of the money-changers and the seats of
-    //      those who sold pigeons. 
+    //        in the temple, and he overturned the tables of the money-changers and the seats
+    //        of those who sold pigeons. 
     //      Mathew 21:13  He said to them, 
-    //      “It is written, ‘My house shall be called a house of prayer,’
-    //      but you make it a den of robbers.” 
+    //        “It is written, ‘My house shall be called a house of prayer,’
+    //        but you make it a den of robbers.” 
+    //
+    //    b) Also within Luke 22:35-38 when Jesus instructs his disciples to buy 2 swords.
+    //       He tells them to sell their cloaks to obtain one.
+    //       Luke 22:38  And they said, “Look, Lord, here are two swords.” 
+    //         And he said to them, “It is enough.” 
+    //       Jesus is referring the 2 edged sword in Revelation 1:16 which is the Word of God.
+    //       The 2 swords are the New and Old Testaments.
+    //
+    //       As I will show you, that I had to use both swords (Old & New Testaments) to decipher the 
+    //       timeline hidden in the bible.
+    
 
     // 2) Jesus knew that from John 11:1 to John 11:35, that this pattern of (35) elapsed
     //    would be found tragic 11/1 event will be fulfilled in the future.
