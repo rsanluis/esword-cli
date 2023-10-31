d70aa6899eb8d84b7744c0924ae3ec719fd529a1
Roland San Luis <rsanluis@gmail.com>
2023-10-30 22:21:50 -0400

    ref

--- src/test/java/com/codified/esword/dao/PropheticClock_Pattern_23_Becoming_2520.java
+++ src/test/java/com/codified/esword/dao/PropheticClock_Pattern_23_Becoming_2520.java
@@ -6,6 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class PropheticClock_Pattern_23_Becoming_2520 {
     // TODO: pattern 23 transitioning into 2520 test
 
-    
+
+    // 
+
+    // https://github.com/rsanluis/esword-cli/blob/main/images/patterns/pattern_105_adonai_doubled.png
+
+    // https://github.com/rsanluis/esword-cli/blob/main/images/patterns/I_or_ME_pattern.png
+
+    // https://github.com/rsanluis/esword-cli/blob/main/images/patterns/pattern_53.png
     
 }
