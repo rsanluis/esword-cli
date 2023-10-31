8203a193f249d250fc576a97900becf0caa77c30
Roland San Luis <rsanluis@gmail.com>
2023-10-31 01:39:47 -0400

    ref

--- src/test/java/com/codified/esword/dao/PropheticClock_Pattern_23_Becoming_2520.java
+++ src/test/java/com/codified/esword/dao/PropheticClock_Pattern_23_Becoming_2520.java
@@ -6,17 +6,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class PropheticClock_Pattern_23_Becoming_2520 {
     // TODO: pattern 23 transitioning into 2520 test
 
+    // https://raw.githubusercontent.com/rsanluis/esword-cli/main/images/patterns/IAM_times_3.png
 
-    // https://github.com/rsanluis/esword-cli/blob/main/images/patterns/IAM_times_3.png
+    // https://raw.githubusercontent.com/rsanluis/esword-cli/main/images/patterns/pattern_105_adonai_doubled.png
 
-    // https://github.com/rsanluis/esword-cli/blob/main/images/patterns/pattern_105_adonai_doubled.png
+    // https://raw.githubusercontent.com/rsanluis/esword-cli/main/images/patterns/I_or_ME_pattern.png
 
-    // https://github.com/rsanluis/esword-cli/blob/main/images/patterns/I_or_ME_pattern.png
+    // https://raw.githubusercontent.com/rsanluis/esword-cli/main/images/patterns/pattern_53.png
 
-    // https://github.com/rsanluis/esword-cli/blob/main/images/patterns/pattern_53.png
+    // https://raw.githubusercontent.com/rsanluis/esword-cli/main/images/patterns/69_going_onto_70.png
 
-    // https://github.com/rsanluis/esword-cli/blob/main/images/patterns/69_going_onto_70.png
-
-    // https://github.com/rsanluis/esword-cli/blob/main/images/patterns/69_going_onto_70_times_3_pattern_111_discovered.png
-    
+    // https://raw.githubusercontent.com/rsanluis/esword-cli/main/images/patterns/69_going_onto_70_times_3_pattern_111_discovered.png
+ 
+    // https://raw.githubusercontent.com/rsanluis/esword-cli/main/images/patterns/210_going_onto_2520.png
 }
