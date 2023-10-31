845d3c6ec033e613610ea055ee4cb05585f126ef
Roland San Luis <rsanluis@gmail.com>
2023-10-31 10:45:31 -0400

    ref

--- src/test/java/com/codified/esword/dao/PropheticClock_Pattern_LastHalfWeek.java
+++ src/test/java/com/codified/esword/dao/PropheticClock_Pattern_LastHalfWeek.java
@@ -6,4 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class PropheticClock_Pattern_LastHalfWeek {
     // https://raw.githubusercontent.com/rsanluis/esword-cli/main/images/last_half_week/last_half_week.png
     // https://raw.githubusercontent.com/rsanluis/esword-cli/main/images/last_half_week/last_half_week_continued.png
+    // https://www.biblestudy.org/bibleref/meaning-of-numbers-in-bible/126.html
+    // https://raw.githubusercontent.com/rsanluis/esword-cli/main/images/numbers_in_bible/126/appointed_time_126.png
+    // https://raw.githubusercontent.com/rsanluis/esword-cli/main/images/numbers_in_bible/126/appointed_time_chart_126_of_20.png
 }
