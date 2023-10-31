d628a3bc6baeba41ff1976682eea5a62fcd16725
Roland San Luis <rsanluis@gmail.com>
2023-10-31 18:02:08 -0400

    ref

--- src/test/java/com/codified/esword/dao/TheDifferentWaysGodWarnsUsAboutTheFloodTest.java
+++ src/test/java/com/codified/esword/dao/TheDifferentWaysGodWarnsUsAboutTheFloodTest.java
@@ -4,27 +4,60 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class TheDifferentWaysGodWarnsUsAboutTheFloodTest {
-    // 
     
     // Revelation 18:21  And a mighty angel took up a stone like a great millstone, 
     // and cast it into the sea, saying, Thus with violence shall that great city 
     // Babylon be thrown down, and shall be found no more at all.
     
-    
-    // Mat 24:37  But as the days of Noe were, so shall also the coming of the Son of man be. 
-    // Mat 24:38  For as in the days that were before the flood they were eating and drinking, marrying and giving in marriage, until the day that Noe entered into the ark, 
-    // Mat 24:39  And knew not until the flood came, and took them all away; so shall also the coming of the Son of man be. 
-    // Mat 24:40  Then shall two be in the field; the one shall be taken, and the other left. 
-    // Mat 24:41  Two women shall be grinding at the mill; the one shall be taken, and the other left. 
-    // Mat 24:42  Watch therefore: for ye know not what hour your Lord doth come. 
+    // Rev 18:2  And he cried mightily with a strong voice, saying, Babylon the great is fallen,
+    // is fallen, and is become the habitation of devils, and the hold of every foul spirit, and
+    // a cage of every unclean and hateful bird. 
 
-    // The key to finding out what will happen is to be watchful and alert as Jesus said in 
-    // Matthew 24:42
+    // *** Flood/water event ***   
+    // Matthew 24:37  But as the days of Noe were, so shall also the coming of the Son of man be. 
+    // Matthew 24:38  For as in the days that were before the flood they were eating and drinking, marrying and giving in marriage, until the day that Noe entered into the ark, 
+    // Matthew 24:39  And knew not until the flood came, and took them all away; so shall also the coming of the Son of man be. 
+    // Matthew 24:40  Then shall two be in the field; the one shall be taken, and the other left. 
+    // Matthew 24:41  Two women shall be grinding at the mill; the one shall be taken, and the other left. 
+    // Matthew 24:42  Watch therefore: for ye know not what hour your Lord doth come. 
 
+    // *** Water event ***
+    // Matthew 18:6  But whoso shall offend one of these little ones which believe in me, it were better
+    // for him that a millstone were hanged about his neck, and that he were drowned in the depth of the sea. 
 
+    // *** Go up to the mountain and build your house to glorify God ***
+    // Hagai 1:8  Go up to the mountain, and bring wood, and build the house; and I will take pleasure in it, 
+    //   and I will be glorified, saith the LORD. 
+    // Hagai 2:6  For thus saith the LORD of hosts; Yet once, it is a little while, and I will shake the heavens,
+    //   and the earth, and the sea, and the dry land; 
+    // Hagai 2:7  And I will shake all nations, and the desire of all nations shall come: and I will fill 
+    //   this house with glory, saith the LORD of hosts. 
 
-    // * These little ones & millstone around your neck warning
-    // * Hagai 1 & 2
+    // Build Your House on the Rock
+    // Mat 7:24  Therefore whosoever heareth these sayings of mine, and doeth them, I will liken him unto a 
+    //   wise man, which built his house upon a rock: 
+    // Mat 7:25  And the rain descended, and the floods came, and the winds blew, and beat upon that house;
+    //   and it fell not: for it was founded upon a rock. 
+    // Mat 7:26  And every one that heareth these sayings of mine, and doeth them not, shall be likened unto 
+    //   a foolish man, which built his house upon the sand: 
+    // Mat 7:27  And the rain descended, and the floods came, and the winds blew, and beat upon that house; 
+    //   and it fell: and great was the fall of it. 
+
+    // Luk 6:48  He is like a man which built an house, and digged deep, and laid the foundation on a rock: and when the flood arose, the stream beat vehemently upon that house, and could not shake it: for it was founded upon a rock. 
+
+    // Daniel 9:26  And after threescore and two weeks shall Messiah be cut off, but not for himself:
+    // and the people of the prince that shall come shall destroy the city and the sanctuary; and the
+    // end thereof shall be with a flood, and unto the end of the war desolations are determined.
+
+    // Jeremiah 51:42  The sea is come up upon Babylon: she is covered with the multitude of the waves thereof.
+
+    // Jeremiah 51:55  Because the LORD hath spoiled Babylon, and destroyed out of her the great voice; when her waves 
+    // do roar like great waters, a noise of their voice is uttered:
+
+    // Isaiah 5:30  And in that day they shall roar against them like the roaring of the sea: 
+    //   and if one look unto the land, behold darkness and sorrow, and the light is 
+    //   darkened in the heavens thereof. 
+
     
     // God's names and numbers (required)
     // * Revelation 12 sign
@@ -38,9 +71,7 @@ public class TheDifferentWaysGodWarnsUsAboutTheFloodTest {
 
     // * H2555
     // * John 1:1-35 - people are sick - Lazarus
-    // Haggai 1 & 2
-    // * arrows sharpened
     
 
-    // Luk 6:48  He is like a man which built an house, and digged deep, and laid the foundation on a rock: and when the flood arose, the stream beat vehemently upon that house, and could not shake it: for it was founded upon a rock. 
+
 }
