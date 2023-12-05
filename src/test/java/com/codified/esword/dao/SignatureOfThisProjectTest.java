8d914ad5d54466e41260e7ef95f93b0daea9f028
Roland San Luis <rsanluis@gmail.com>
2023-12-05 10:24:28 -0500

    ref

--- src/test/java/com/codified/esword/dao/SignatureOfThisProjectTest.java
+++ src/test/java/com/codified/esword/dao/SignatureOfThisProjectTest.java
@@ -263,9 +263,9 @@ public class SignatureOfThisProjectTest {
         // https://www.biblestudy.org/bibleref/meaning-of-numbers-in-bible/20.html
         //
         // The number 20 conveys a meaning of a complete or perfect waiting period.
-        // https://github.com/rsanluis/esword-cli/blob/main/images/my_signature/20/20_perfect_waiting_period.png
+        // https://raw.githubusercontent.com/rsanluis/esword-cli/main/images/my_signature/20/20_perfect_waiting_period.png
         //
-        // https://github.com/rsanluis/esword-cli/blob/main/images/numbers_in_bible/126/appointed_time_chart_126_of_20.png
+        https://raw.githubusercontent.com/rsanluis/esword-cli/main/images/numbers_in_bible/126/appointed_time_chart_126_of_20.png
         // Chart generated using https://www.meta-chart.com/
         //
         // Ezekiel 33:6  But if the watchman see the sword come, and blow not the trumpet,
