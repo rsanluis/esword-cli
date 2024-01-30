b90b0107a402067bea407962671ff7a6873f03d9
Roland San Luis <rsanluis@gmail.com>
2024-01-30 08:29:49 -0500

    adding testSearchByKeyword_G5140_AndContext_Bible

--- src/test/java/com/codified/esword/dao/SearchDAOImpl_69_Matches_Test.java
+++ src/test/java/com/codified/esword/dao/SearchDAOImpl_69_Matches_Test.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,5 +32,31 @@ public class SearchDAOImpl_69_Matches_Test {
         log.info("matches: {}", matches);
         assertTrue(verses == 66);
         assertTrue(matches == 69);
+    }
+
+    // G5140
+    // τρεῖς, τρία
+    // treis    tria
+    // trice, tree'-ah
+    // A primary (plural) number; “three”: - three.
+    // Total KJV occurrences: 69
+    @Test
+    void testSearchByKeyword_G5140_AndContext_Bible() {
+        List<SearchResult> resultsList = searchDAO.searchByKeywordAndContext("g5140", "Bible");
+        int verses = 0;
+        int matches = 0;
+        matches += BibleUtils.countMatches(resultsList);
+        verses = resultsList.size();
+        log.info("verses:  {}", verses);
+        log.info("matches: {}", matches);
+        assertTrue(verses == 60);
+        assertTrue(matches == 69);
+        // 69 divided by 3 = 23 which stands for "I AM"
+        // 60 divided by 3 = 20 which stands for the "waiting period"
+        // https://www.biblestudy.org/bibleref/meaning-of-numbers-in-bible/20.html
+        // 2520 divided by 20 = 126 
+        // The English word "appointed" is listed 126 times in 126 King James Bible verses.
+        // https://www.biblestudy.org/bibleref/meaning-of-numbers-in-bible/126.html
+        // "Appointed" time
     }
 }
